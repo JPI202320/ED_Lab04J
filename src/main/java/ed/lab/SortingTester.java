2ab7cb8b1de2712498b4d4c578cb62c4493cc4e0
JPI202320 <jeimypalencia20@gmail.com>
2026-02-09 20:55:30 -0600

    EJERCICIO 2

--- src/main/java/ed/lab/SortingTester.java
+++ src/main/java/ed/lab/SortingTester.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SortingTester<T extends Comparable<T>> {
+public class    SortingTester<T extends Comparable<T>> {
     private static final int ARRAY_SIZE = 10000;
     private static final int TEST_SIZE = 1000;
 
@@ -31,9 +31,10 @@ public class SortingTester<T extends Comparable<T>> {
                 .average()
                 .orElse(0);
 
+
         long sum = durations.stream()
                 .mapToLong(Duration::toMillis)
-                .sum(); // hola
+                .sum();
 
         System.out.printf("\t\tTiempo promedio: %s ms\n", average);
         System.out.println("\t\tSumatoria de tiempo: " + (sum));
